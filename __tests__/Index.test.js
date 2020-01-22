e4edadfb2387c8d1c31d4f06d9bcc19a26fa2661
Nigel Reoch <nreoch9@gmail.com>
2020-01-22 11:24:26 -0500

    Added test

--- __tests__/Index.test.js
+++ __tests__/Index.test.js
@@ -1,4 +1,9 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import IndexPage from "../pages/index";
 
-test("redners without error", () => {});
+test("renders without error", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<IndexPage />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
